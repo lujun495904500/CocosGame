dced4967d7d26d4992d39535e00b8fd1b9734c7f
Lujun <45997937+lujun495904500@users.noreply.github.com>
2019-09-29 11:18:49 +0800

    Update boot.lua

--- game/boot/src/boot.lua
+++ game/boot/src/boot.lua
@@ -218,7 +218,7 @@ function UpdateScene:beginUpdate()
 
 								printInfo("reboot ...")
 					
-								utils.remove_module("config", "cocos[.]*", "app[.]*")
+								utils.remove_module("^config$", "^cocos[.].*$", "^app[.].*$")
 								dofile("boot.lua", true)
 							else
 								local config = ...
